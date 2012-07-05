382a1ef6b6ea9c43d49fa919ab15bf8392670849
Cole Ott <cole.ott@gmail.com>
2012-07-04 19:23:15 -0700

    init'd Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modernprocess::Application.config.secret_token = '5a050ba776c32c67216cb78709fe4d39fea368944aea63fddbf84c54eed3f7c031c7a15eeeae91290327abb7177a9f909d8130693fd574413479fd3d5717a48c'
